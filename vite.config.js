92cf443fed9c6a7f207e17e391692505fe9574f0
AdityaY1612 <110178790+AdityaY1612@users.noreply.github.com>
2023-10-04 20:27:15 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  build: {
+    outDir: 'dist',
+    assetsDir: '.',
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`
+      }
+    }
+  }
+}
